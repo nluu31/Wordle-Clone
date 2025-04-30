790703f3d8038abe622c0ccf6a00fcfb6c2880e5
nluu <mrnick731@gmail.com>
2025-04-30 15:26:25 -0700

    added winner and loser

--- wordle.js
+++ wordle.js
@@ -1,13 +1,15 @@
-const answer = "squid";
+const wordAnswer = "squid";
 
 let row = 0;
 let col = 0;
+let gameOver = false;
+
 
 const alpha = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
     'u', 'v', 'w', 'x', 'y', 'z'
-  ];
+];
 
 function makeBox(container, row, col) {
     const box = document.createElement("div");
@@ -42,12 +44,18 @@ function enterKey() {
 
         if (key == "Backspace") {
             deleteLetter();
-        } else if (alpha.includes(key)){
+        } else if (alpha.includes(key) && col != 5) {
             addLetter(key);
         }
+        if (key == "Enter" && col == 5) {
+            update();
+            row++;
+            col = 0;
 
+            const curr = document.getElementById("box" + row + col);
+            test(curr);
+        }
         document.getElementById("test").textContent = row + "," + col;
-
 
     }
 }
@@ -57,37 +65,46 @@ function addLetter(key) {
         const curr = document.getElementById("box" + row + col);
         curr.textContent = key;
         test(curr);
-        if (col == 4) {
-            row++;
-            col = 0;
-        } else {
-            col++;
-        }
+        col++;
     }
 }
 function deleteLetter() {
     if (col > 0) {
         col--;
     }
-    else if (col == 0 && row > 0) {
-            row--;
-            col = 4;
-        }
-        const curr = document.getElementById("box" + row + col);
-        curr.textContent = '';
-        test(curr);
+    const curr = document.getElementById("box" + row + col);
+    curr.textContent = '';
+    test(curr);
+}
+
+
+
+function test(curr) {
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = '';
+    })
+    curr.style.backgroundColor = 'black';
+}
+
+function update() {
+    const word = document.getElementById("currWord");
+    let answer = "";
+    for (let x = 0; x < 5; x++) {
+        const letter = document.getElementById("box" + row + x).textContent;
+        answer += letter;
+    }
+    if (answer == wordAnswer) {
+        word.textContent = "WINNER!!!";
+        // DO SOMETHING THAT STOPS THE GAME
+    }
+    if (answer != wordAnswer && row == 5) {
+        word.textContent = "GAME OVER, U SUCK";
+        // DO SOMETHING THAT ENDS THE GAME
     }
 
+}
 
-
-    function test(curr) {
-        const boxes = document.querySelectorAll('.box');
-        boxes.forEach(box => {
-            box.style.backgroundColor = ''; 
-        })
-        curr.style.backgroundColor = 'black';
-    }
-    
 start();
 enterKey();
 
